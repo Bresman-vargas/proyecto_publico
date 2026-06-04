b792e1c27fabeacb191848bcdd3d49ccb7e90865
Vicente Mery <vicentemeryarancibia@gmail.com>
2026-06-03 23:32:39 -0400

    a

--- apps/frontend/src/pages/surveys/SurveyForm.tsx
+++ apps/frontend/src/pages/surveys/SurveyForm.tsx
@@ -169,7 +169,6 @@ export default function SurveyForm() {
             )}
           </section>
 
-
           <div className="py-4 col-span-2">
             <button
               className={`rounded-md px-4 py-2 w-full ${
